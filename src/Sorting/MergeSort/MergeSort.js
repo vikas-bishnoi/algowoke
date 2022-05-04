1c9cafdccb4e722f8c82ee7d5e224ccde2384fd5
Vikas Bishnoi <vikaskbm@gmail.com>
2022-05-05 03:43:08 +0530

    finish setting up sorting files

--- src/Sorting/MergeSort/MergeSort.js
+++ src/Sorting/MergeSort/MergeSort.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import "./MergeSort.css";
 
+
+const PRIMARY_COLOR = 'teal';
+
+
 const MergeSort = ({ props }) => {
   const [array, setArray] = useState([]);
 
@@ -8,20 +12,46 @@ const MergeSort = ({ props }) => {
     return Math.floor(Math.random() * (max - min + 1))
   }
   
-  useEffect(() => {
-    for(let i=0; i<100; i++) {
-      array.push(randomBetween(5, 1000))
+  const resetArray = () => {
+    let temp = []
+    for(let i=0; i<300; i++) {
+      temp.push(randomBetween(5, 550))
     }
+    setArray(temp);
+  }
+
+  const mergeSort = () => {}
+
+  const quickSort = () => {}
+
+  const heapSort = () => {}
+
+  const bubbleSort = () => {}
+
+  useEffect(() => {
+    resetArray()
   }, []);
 
   return (
-    <>
+    <div className="array-container">
       {array.map((value, idx) => {
-        <div className="array-bar" ket={idx}>
-          {value}
-        </div>;
+        return (
+          <div
+            className="array-bar"
+            key={idx}
+            style={{
+              backgroundColor: PRIMARY_COLOR,
+              height: `${value}px`,
+            }}></div>
+        )
       })}
-    </>
+      
+      <button onClick={resetArray}>Reset Array</button>
+      <button onClick={mergeSort}>Merge Sort</button>
+      <button onClick={quickSort}>Quick Sort</button>
+      <button onClick={heapSort}>Heap Sort</button>
+      <button onClick={bubbleSort}>Bubble Sort</button>
+    </div>
   );
 };
 
