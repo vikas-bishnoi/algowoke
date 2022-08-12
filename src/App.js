465b88971b87cbf092788909a9c33841c50cf904
Vikas Bishnoi <vikaskbm@gmail.com>
2022-08-12 07:26:15 +0530

    final. for now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
-import MergeSort from './Sorting/MergeSort/MergeSort';
-import QuickSort from './Sorting/QuickSort/QuickSort';
-import BubbleSort from './Sorting/BubbleSort/BubbleSort';
+import "./App.css";
+import MergeSort from "./Sorting/MergeSort/MergeSort";
+import QuickSort from "./Sorting/QuickSort/QuickSort";
+import BubbleSort from "./Sorting/BubbleSort/BubbleSort";
+import { InsertionSort } from "./Sorting/InsertionSort/InsertionSort";
 
 function App() {
   return (
     <div className="App">
-      <BubbleSort/>
+      <MergeSort />
     </div>
   );
 }
