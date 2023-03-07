4f365069cdd4f3fd19b204c5b776287668732c8b
vikaskbm <vikaskbm@gmail.com>
2023-03-07 18:39:07 +0530

    Add selection sort file and styling

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import MergeSort from "./Sorting/MergeSort/MergeSort";
 import QuickSort from "./Sorting/QuickSort/QuickSort";
 import BubbleSort from "./Sorting/BubbleSort/BubbleSort";
 import InsertionSort from "./Sorting/InsertionSort/InsertionSort";
+import SelectionSort from "./Sorting/SelectionSort/SelectionSort";
 
 function App() {
   return (
     <div className="App">
-      <InsertionSort />
+      <SelectionSort />
     </div>
   );
 }
---
+++ src/Sorting/SelectionSort/SelectionSort.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SelectionSort = () => {
+  return (
+    <div>SelectionSort</div>
+  )
+}
+
+export default SelectionSort
