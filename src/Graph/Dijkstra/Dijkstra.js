e1d28abd1d0cdd3495dcfe3df77f3c498df09bb5
vikaskbm <vikaskbm@gmail.com>
2023-03-08 02:05:28 +0530

    Add helper methods to create a grid

--- src/Graph/Dijkstra/Dijkstra.js
+++ src/Graph/Dijkstra/Dijkstra.js
@@ -1,17 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import Square from '../Components/Square';
 
+const SOURCE_ROW = 4;
+const SOURCE_COL = 4;
+const DESTINATION_ROW = 7;
+const DESTINATION_COL = 25;
+
 const Dijkstra = () => {
   const [grid, setGrid] = useState([]);
-  useEffect(() => {
+
+  const createSquare = (row, col) => {
+    return {
+        row,
+        col,
+        isVisited: false
+    }
+  }
+
+  const createGrid = () => {
     const newGrid = [];
     for(let i=1; i <= 10; i++) {
         const row = [];
         for(let j=0; j<=30; j++) {
-            row.push([]);
+            row.push(createSquare(i, j));
         }
         newGrid.push(row);
     }
+    return newGrid;
+  }
+
+  useEffect(() => {
+    const newGrid = createGrid();
     setGrid(newGrid);
   }, []);
 
