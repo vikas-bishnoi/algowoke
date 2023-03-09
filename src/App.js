ee85b16b71b6a11b55570726fffb14da9bf56554
vikaskbm <vikaskbm@gmail.com>
2023-03-09 22:46:39 +0530

    Add graph component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import "./App.css";
-import MergeSort from "./Sorting/MergeSort/MergeSort";
-import QuickSort from "./Sorting/QuickSort/QuickSort";
-import BubbleSort from "./Sorting/BubbleSort/BubbleSort";
-import InsertionSort from "./Sorting/InsertionSort/InsertionSort";
-import SelectionSort from "./Sorting/SelectionSort/SelectionSort";
-import Dijkstra from "./Graph/Dijkstra/Dijkstra";
-import Home from "./Home/Home";
-import Navbar from "./Home/Navbar";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link,
 } from "react-router-dom";
+
+import Home from "./Home/Home";
+import Navbar from "./Home/Navbar";
+import Graph from './Graph/Graph';
+import Sorting from './Sorting/Sorting';
+
+import "./App.css";
 
 function App() {
   return (
@@ -21,6 +18,8 @@ function App() {
     <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/graph-algo" element={<Graph />} />
+        <Route path="/sort-algo" element={<Sorting />} />
       </Routes>
     </Router>
     </>
