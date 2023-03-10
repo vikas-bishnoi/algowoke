d9952c546b7ab0061431d5a2d036be40ba187f7f
vikaskbm <vikaskbm@gmail.com>
2023-03-10 05:44:01 +0530

    Create sidebar components

---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Sidebar.css'
+
+
+const SideBar = () => {
+    return (
+      <div className="sidebar">
+        <div className="sidebar-header">
+            <p>lorem10 </p>
+            <p>lorem10 </p>
+            <p>lorem10 </p>
+            <p>lorem10 </p>
+        </div>
+      </div>
+    );
+}
+
+export default SideBar;
