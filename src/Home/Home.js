fff1a0f4ca6e342ae4ecc9099585703caa7d3de8
vikaskbm <vikaskbm@gmail.com>
2023-03-12 21:04:31 +0530

    Add home page styles

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import SideBar from '../Components/Sidebar/Sidebar'
+import React from "react";
 
 const Home = () => {
   return (
-    <div className='main'>
-      <div>Home</div>
-    </div>
-  )
-}
+    <>
+      <div
+        id="home"
+        style={{
+          backgroundImage: `url(https://static.pexels.com/photos/232/apple-iphone-books-desk-large.jpg)`,
+        }}
+        className=" bg-center bg-cover bg-no-repeat h-90 flex flex-col "
+      >
+        <div className="wrapper flex justify-between items-center h-screen w-full px-20 lg:justify-center lg:px-6">
+          <div className="content lg:text-center">
+            <div className="btn py-3">
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
