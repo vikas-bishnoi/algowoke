720c7a21bd47ae54d09ab70d0dd9dea2eb808919
vikaskbm <vikaskbm@gmail.com>
2023-03-12 21:04:01 +0530

    Discard extra nav links

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,34 +1,35 @@
-import React from 'react'
+import React from "react";
 import { NavLink, Link } from "react-router-dom";
 
-import './Navbar.css'
+import "./Navbar.css";
 const Navbar = () => {
   return (
     <div className="navbar">
-            <Link to="/">
-        <div className="logo">
-            AlgoWoke
-        </div>
-            </Link>
-        <div className="menu">
-            <NavLink to="/graph-algo" className={({ isActive }) =>
-              `link ${isActive ? 'link active' : ''}`
-            }>
-                <div className='title'>Graph</div>
-            </NavLink>
-            <NavLink to="/sort-algo" className={({ isActive }) =>
-              `link ${isActive ? 'link active' : ''}`
-            }>
-                <div className='title'>Sorting</div>
-            </NavLink>
-            <NavLink to="/search-algo" className={({ isActive }) =>
-              `link ${isActive ? 'link active' : ''}`
-            }>
-                <div className='title'>Searching</div>
-            </NavLink>
-        </div>
+      <Link to="/">
+        <div className="logo">AlgoWoke</div>
+      </Link>
+      <div className="menu">
+        {/* <NavLink
+          to="/graph-algo"
+          className={({ isActive }) => `link ${isActive ? "link active" : ""}`}
+        >
+          <div className="title">Graph</div>
+        </NavLink> */}
+        <NavLink
+          to="/sort-algo"
+          className={({ isActive }) => `link ${isActive ? "link active" : ""}`}
+        >
+          <div className="title">Sorting</div>
+        </NavLink>
+        {/* <NavLink
+          to="/search-algo"
+          className={({ isActive }) => `link ${isActive ? "link active" : ""}`}
+        >
+          <div className="title">Searching</div>
+        </NavLink> */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
